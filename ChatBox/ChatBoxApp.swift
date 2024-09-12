fab0c46b2cccca16deadb3d0fb27c24ff10b7591
Lei Liu <lei.liu@autodesk.com>
2024-09-12 13:41:51 +0800

    Initial Commit

---
+++ ChatBox/ChatBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBoxApp.swift
+//  ChatBox
+//
+//  Created by Lei Liu on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
